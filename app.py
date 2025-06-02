fdb7e0b1fb722b0887c613ec31f843241a335530
Sigurd313 <ssigurdr@gmail.com>
2025-06-02 17:13:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ def index():
     return "yt-dlp server is running"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
